fcdf142755a13b68a9fccbeb3b6715198b1ba6b2
Joris <jote0002@stud.kea.dk>
2024-04-22 14:03:48 +0200

    fået farveskift status til at virke

--- src/js/test.js
+++ src/js/test.js
@@ -1,11 +1,28 @@
 window.addEventListener("load", sidenVises);
 const clickable = document.querySelectorAll;
-const klikbar = document.getElementsByClassName("p_piller");
 
 function sidenVises() {
-  kondom.addEventListener("mousedown", clicked);
-  piller.addEventListener("mousedown", clicked);
-  klikbar.addEventListener("mousedown", clicked);
+  // desktop
+  p1.addEventListener("mousedown", clicked);
+  k1.addEventListener("mousedown", clicked);
+  t1.addEventListener("mousedown", clicked);
+  p2.addEventListener("mousedown", clicked);
+  c1.addEventListener("mousedown", clicked);
+  l1.addEventListener("mousedown", clicked);
+  b1.addEventListener("mousedown", clicked);
+  s1.addEventListener("mousedown", clicked);
+  s2.addEventListener("mousedown", clicked);
+
+  //   mobile
+  mp1.addEventListener("mousedown", clicked);
+  mk1.addEventListener("mousedown", clicked);
+  mt1.addEventListener("mousedown", clicked);
+  mp2.addEventListener("mousedown", clicked);
+  mc1.addEventListener("mousedown", clicked);
+  ml1.addEventListener("mousedown", clicked);
+  mb1.addEventListener("mousedown", clicked);
+  ms1.addEventListener("mousedown", clicked);
+  ms2.addEventListener("mousedown", clicked);
 }
 
 function clicked() {
