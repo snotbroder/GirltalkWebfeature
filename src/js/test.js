0c0092ade747b79ef39e07a472095b9390d1e50e
Joris <jote0002@stud.kea.dk>
2024-04-22 13:42:06 +0200

    forsøgt at lave

--- src/js/test.js
+++ src/js/test.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", sidenVises);
 const clickable = document.querySelectorAll;
+const klikbar = document.getElementsByClassName("p_piller");
 
 function sidenVises() {
   kondom.addEventListener("mousedown", clicked);
   piller.addEventListener("mousedown", clicked);
+  klikbar.addEventListener("mousedown", clicked);
 }
 
 function clicked() {
